7079c6118e95e4a6bc8579cf494d535c5bdf6f1b
Dalibor Pavlov <pavlov.dalibor@hotmail.com>
2022-09-05 12:01:32 +0200

    Add files via upload

---
+++ Shapes/Shapes/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Shapes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1 = new Rectangle();
+
+            r1.Enabled = true;
+            r1.Name = "My Rectangle";
+            
+            Console.WriteLine(
+                $"Rectangle: {r1.Name}\n" +
+                $"{r1.SizeAsString}"
+            );
+        }
+    }
+}
---
+++ Shapes/Shapes/Rectangle.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public class Rectangle
+    {
+        // Ein Feld mit einem für Felder typischen Zugriffsmodifizierer
+        private string name;
+        private int width = 15;
+        private int height = 15;
+        private int radius;
+
+        // Ein Feld, dessen Wert während der Lebensdauer eines Objekts nicht verändert werden kann.
+        private readonly int area;
+
+        // Ein Feld, für welches ein Standardwert zugewiesen wird
+        private Color color = System.Drawing.Color.Black;
+
+        // Eine selber implementierte, read-only Eigenschaft.
+        public int Area
+        {
+            get { return width * height; }
+        }
+
+        // Eine selber implementierte, read-write Eigenschaft
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        // Eine selber implementierte, write-only Eigenschaft
+        public int Radius
+        {
+            private get { return radius; }
+            set { radius = value; }
+        }
+
+        // Eine automatisch implementierte, read-only Eigenschaft.
+
+        public int Top { get; }
+        public int Bottom { get; }
+
+        // Eine automatisch implementierte, read-write Eigenschaft
+        public string Color { get; set; }
+
+        // Eine automatisch implementierte, write-only Eigenschaft
+        public bool Enabled { private get; set; }
+
+        // Zwei verschiedene Eigenschaften mit jeweils unterschiedlicher Anwendung der expression body Schreibweise.
+        public bool IsEven
+        {
+            get { return width % 2 == 0 && height % 2 == 0; }
+            set { width = 10; height = 10; }
+        }
+
+        public string RadiusAsString
+        {
+            get => $"Radius: {radius}";
+            set => radius = int.Parse(value);
+        }
+
+        public String SizeAsString
+        {
+            get { return $"Width: {width} | Height: {height}"; }
+            set
+            {
+                string[] sizes = value.Split('|');
+                int start1 = sizes[0].IndexOf(':') + 1;
+                int end1 = sizes[0].Length - 1;
+                width = int.Parse(value.Substring(start1, end1));
+
+                int start2 = sizes[0].IndexOf(':') + 1;
+                int end2 = sizes[0].Length;
+                height = int.Parse(value.Substring(start2, end2));
+            }
+        }
+
+    }
+}
