c95c90bca7582721079aa6882a207f791ce1aa28
Dalibor Pavlov <pavlov.dalibor@hotmail.com>
2022-09-05 11:35:32 +0200

    Add files via upload

---
+++ oop/oop/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace oop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instanziieren einer Klasse (Aus einer Klasse wird ein Objekt)
+            Rectangle rectangle1 = new Rectangle();
+            Rectangle rectangle2 = new Rectangle();
+
+            // Setter
+            rectangle1.Name = "Ben";
+
+            // Getter
+            Console.WriteLine($"{rectangle1.Name}: {rectangle1.Area}");
+            Console.WriteLine($"{rectangle2.Name}: {rectangle2.Area}");
+        }
+    }
+}
---
+++ oop/oop/Rectangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop
+{
+    public class Rectangle
+    {
+        // Fields
+        // private = kein Zugriff ausserhalb (Zugriffsmodifizierer)
+        // Benennung von Fields: camelCase
+        //private string          name;
+        //private readonly int    width;
+        //private readonly int    height;
+        //private Color           color = Color.Black;
+
+
+        // Properties
+        // public props = PascalCase
+        // Property mit BackingField
+        // get; set; Autoproperties
+        public string   Name { get; set; } = "Rectangle";
+        public int      Width { get; } = 15;
+        public int      Height { get; } = 10;
+        public Color    Color { get; set; } = Color.Black;
+
+        public int      Area { get { return Width * Height; } }
+
+
+
+
+
+
+    }
+}
