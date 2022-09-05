61ba7437ea437dd8ca5e4ff2df905599ff66e7b2
Dalibor Pavlov <pavlov.dalibor@hotmail.com>
2022-09-05 11:34:39 +0200

    Add files via upload

---
+++ ggt_kgv/ConsoleApp1/MyMath.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ggtkgv
+{
+    public class MyMath
+    {
+        public static int Calc_ggT(int a, int b)
+        {
+            while(b != 0)
+            {
+                int z = a % b;
+                a = b;
+                b = z;
+            }
+
+            return a;
+        }
+
+        public static int Calc_ggT(int a)
+        {
+
+            return a * 2;
+        }
+
+        public static int Calc_kgV(int a, int b)
+        {
+            return (a / Calc_ggT(a, b)) * b;
+        }
+
+        public static int Calc_ggT_r(int a, int b)
+        {
+            if (b == 0)
+            {
+                return a;
+            } else
+            {
+                return Calc_ggT_r(b, a % b);
+            }
+        }
+
+        public static int MwArr(int[] arr)
+        {
+            return Convert.ToInt32(arr.Average());
+        }
+
+        public static int GwArr(int[] arr)
+        {
+            return arr.Max();
+        }
+
+        public static int KwArr(int[] arr)
+        {
+            return arr.Min();
+        }
+
+        public static void SwapVars(ref int a, ref int b)
+        {
+            int tmp = a;
+            a = b;
+            b = tmp;
+
+        }
+
+        public static void ReverseArray(ref int[] arr)
+        {
+            arr = arr.Reverse().ToArray();
+        }
+
+        public static void SortArray(ref int[] arr)
+        {
+            Array.Sort(arr);
+        }
+    }
+}
---
+++ ggt_kgv/ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿
+namespace ggtkgv
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 13, 22, 3, 41, 20, 43, 9 };
+            Console.WriteLine("Array: [{0}]", string.Join(", ", arr));
+
+            Console.WriteLine("Mittelwert: " + MyMath.MwArr(arr));
+            Console.WriteLine("Kl. Wert: " + MyMath.KwArr(arr));
+            Console.WriteLine("Gr. Wert: " + MyMath.GwArr(arr));
+
+            MyMath.ReverseArray(ref arr);
+            Console.WriteLine("Umgekehrt: [{0}]", string.Join(", ", arr));
+
+            Console.WriteLine("kgV & ggT Rechner: 0 zum Abbruch.");
+
+            for (;;) {
+                int n1 = ReadInt("Zahl 1");
+                if (n1 == -1) { continue; }
+                if (n1 == 0) { break; }
+                int n2 = ReadInt("Zahl 2");
+
+                Console.WriteLine($"{n1} : {n2}\n..swap..");
+                MyMath.SwapVars(ref n1, ref n2);
+                Console.WriteLine($"{n1} : {n2}");
+
+                int ggt = MyMath.Calc_ggT_r(n1, n2);
+                int kgv = MyMath.Calc_kgV(n1, n2);
+
+                Console.WriteLine($"ggT von {n1} und {n2} ist [{ggt}] resp. kgV von {n1} und {n2} ist [{kgv}]\n");
+            }
+
+            Console.WriteLine("Ende");
+        }
+
+        static int ReadInt(string prefix)
+        {
+            Console.Write($"{prefix}: ");
+            int num;
+            if (int.TryParse(Console.ReadLine(), out num))
+            {
+                return num;
+            } else
+            {
+                return -1;
+            }
+            //try
+            //{
+            //     num = Convert.ToInt32(Console.ReadLine());
+            //} catch (Exception e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //}
+        }
+    }
+}
