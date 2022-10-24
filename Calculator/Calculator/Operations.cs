d693e874aa967ba08f93cbfcda802d046ab6aff6
Dalibor Pavlov <d4p4v@hotmail.com>
2022-10-24 08:26:01 +0200

    Add A4

---
+++ Calculator/Calculator/Operations.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Operations
+    {
+        private static int lastResult  = 0;
+        
+        public static int Add(params int[] nums)
+        {
+            int result = 0;
+
+            if (nums.Length <= 1)
+            {
+                result = lastResult + nums[0];
+            } else
+            {
+                foreach(int num in nums)
+                {
+                    result += num;
+                }
+            }
+
+            
+            lastResult = result;
+            return result;
+        }
+
+        public static int Subtract(params int[] nums)
+        {
+            int result = nums[0];
+
+            if (nums.Length <= 1)
+            {
+                result = lastResult - nums[0];
+            }
+            else
+            {
+                foreach (int num in nums.Skip(1))
+                {
+                    result -= num;
+                }
+            }
+
+
+            lastResult = result;
+            return result;
+        }
+
+        public static int Multiply(params int[] nums)
+        {
+            int result = nums[0];
+
+            if (nums.Length <= 1)
+            {
+                result = lastResult * nums[0];
+            }
+            else
+            {
+                foreach (int num in nums.Skip(1))
+                {
+                    result *= num;
+                }
+            }
+
+
+            lastResult = result;
+            return result;
+        }
+
+        public static int Divide(params int[] nums)
+        {
+            int result = nums[0];
+
+            if (nums.Length <= 1)
+            {
+                result = lastResult / nums[0];
+            }
+            else
+            {
+                foreach (int num in nums.Skip(1))
+                {
+                    result /= num;
+                }
+            }
+
+
+            lastResult = result;
+            return result;
+        }
+    }
+}
---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(;;)
+            {
+                int n1 = ReadInt("First number");
+                int n2 = ReadInt("Second number");
+
+                Operations.Add(n1, n2);
+                Operations.Subtract(n1, n2);
+                Operations.Multiply(n1, n2);
+                Operations.Divide(n1, n2);
+            }
+        }
+
+        static int ReadInt(string prefix)
+        {
+            Console.Write($"[PROMPT] :: {prefix} :: ");
+            int buffer;
+            if(int.TryParse(Console.ReadLine(), out buffer))
+            {
+                return buffer;
+            } else
+                Console.WriteLine($"[ERR] :: Must be a number. :: Try again.");
+
+            return ReadInt(prefix);
+        }
+    }
+}
